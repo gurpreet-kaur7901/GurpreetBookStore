679bef57a0668399eb4ee00bc00bcb80893bae80
W0824816 <W0824816@myscc.ca>
2023-10-31 14:37:34 -0400

    new projects

---
+++ GurpreetBooks.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GurpreetBooks.Models
+{
+    public class Class1
+    {
+    }
+}
---
+++ GurpreetBooks.Utility/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace GurpreetBooks.Utility
+{
+    public class Class1
+    {
+    }
+}
