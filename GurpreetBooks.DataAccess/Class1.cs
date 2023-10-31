12d20c9bcab23583217fe9f2bb0efcd9db5036f0
W0824816 <W0824816@myscc.ca>
2023-10-31 16:56:25 -0400

    delete class

--- GurpreetBooks.DataAccess/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GurpreetBooks.DataAccess
-{
-    public class Class1
-    {
-    }
-}
--- GurpreetBooks.Utility/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace GurpreetBooks.Utility
-{
-    public class Class1
-    {
-    }
-}
